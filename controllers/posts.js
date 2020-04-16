5772ef5462d8d36a1c53cadc1a184105788f52d5
Razor <alexandergray37@gmail.com>
2020-04-16 17:08:58 -0400

    users can comment

--- controllers/posts.js
+++ controllers/posts.js
@@ -40,7 +40,7 @@ app.get('/posts/new', (req,res) => res.render('post-new'));
 
 app.get("/posts/:id", function(req, res) {
   // LOOK UP THE POST
-  Post.findById(req.params.id).lean()
+  Post.findById(req.params.id).lean().populate('comments')
     .then(post => {
       res.render("posts-show", { post });
     })
